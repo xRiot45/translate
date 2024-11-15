e92e08b2147c6d2cf291f7ec778c80ce4d0ef747
thomasalbertoDev <12221355@bsi.ac.id>
2024-11-15 22:59:40 +0700

    Update

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from googletrans import Translator
+from deep_translator import GoogleTranslator
 import os
 from tqdm import tqdm
 
@@ -10,25 +10,22 @@ results_folder = 'results'
 # Membuat folder results jika belum ada
 os.makedirs(results_folder, exist_ok=True)
 
-# Tentukan path file yang akan di terjemahkan
-file_path = os.path.join(data_folder, 'theme.csv')
+# Tentukan path file
+file_path = os.path.join(data_folder, 'other.csv')
 
 # Load the file
 data = pd.read_csv(file_path)
 
-# Initialize translator
-translator = Translator()
-
 # Menambahkan animasi loading menggunakan tqdm
-tqdm.pandas(desc="Translate text...")
+tqdm.pandas(desc="Menerjemahkan teks...")
 
 # Translate 'en' column to Indonesian dengan progres bar
-data['id'] = data['en'].progress_apply(lambda x: translator.translate(x, src='en', dest='id').text if pd.notna(x) else x)
+data['id'] = data['en'].progress_apply(lambda x: GoogleTranslator(source='en', target='id').translate(x) if pd.notna(x) else x)
 
-# Tentukan path untuk hasil terjemahan dan nama file hasil terjemahan
+# Tentukan path untuk hasil terjemahan
 translated_file_path = os.path.join(results_folder, 'results.csv')
 
 # Save the translated file
 data.to_csv(translated_file_path, index=False)
 
-print("The translation is complete and the file is saved in:", translated_file_path)
+print("Terjemahan selesai dan file disimpan di:", translated_file_path)

f05d8a9985190a4b0130c426d48389ac075cefbf
thomasalbertoDev <12221355@bsi.ac.id>
2024-11-15 21:00:42 +0700

    Update Program

---
+++ README.md
@@ -0,0 +1,25 @@
+# Translate Tool
+
+## Installation
+```bash
+# Step 1
+git clone https://github.com/xRiot45/translate.git
+
+# Step 2
+cd translate
+
+# Step 3
+sudo su
+
+# Step 4
+python3 -m venv .venv
+
+# Step 5
+source .venv/bin/activate
+
+# Step 6
+pip install -r requirements.txt
+```
+
+## Usage
+Type ```python3 main.py``` to run this tool
--- main.py
+++ main.py
@@ -1,16 +1,34 @@
 import pandas as pd
 from googletrans import Translator
+import os
+from tqdm import tqdm
+
+# Menentukan folder data dan results
+data_folder = 'data'
+results_folder = 'results'
+
+# Membuat folder results jika belum ada
+os.makedirs(results_folder, exist_ok=True)
+
+# Tentukan path file yang akan di terjemahkan
+file_path = os.path.join(data_folder, 'theme.csv')
 
 # Load the file
-file_path = 'theme.csv'
 data = pd.read_csv(file_path)
 
 # Initialize translator
 translator = Translator()
 
-# Translate 'en' column to Indonesian
-data['id'] = data['en'].apply(lambda x: translator.translate(x, src='en', dest='id').text if pd.notna(x) else x)
+# Menambahkan animasi loading menggunakan tqdm
+tqdm.pandas(desc="Translate text...")
+
+# Translate 'en' column to Indonesian dengan progres bar
+data['id'] = data['en'].progress_apply(lambda x: translator.translate(x, src='en', dest='id').text if pd.notna(x) else x)
+
+# Tentukan path untuk hasil terjemahan dan nama file hasil terjemahan
+translated_file_path = os.path.join(results_folder, 'results.csv')
 
 # Save the translated file
-translated_file_path = 'results.csv'
 data.to_csv(translated_file_path, index=False)
+
+print("The translation is complete and the file is saved in:", translated_file_path)
